360a6350482163e056c638119e723a62c044c9ed
Ilia Mediankin <i.mediankin@gmail.com>
2022-12-17 17:43:54 +0300

    feat: add Task7

---
+++ Lesson_01/Task7/Program.cs
@@ -0,0 +1,28 @@
+﻿// Task7. Напишите программу, которая принимает на вход трёхзначное число и 
+//        на выходе показывает **последнюю** цифру этого числа.
+//        Например: 456 -> 6, 782 -> 2, 918 -> 8
+
+using static System.Console;
+
+int n, digits, result;
+string input = "";
+
+Clear();
+
+while (true)
+{
+    Write("Введите: 'q'-выход, или 'трехзначное число', чтобы вывести последнюю цифру этого числа: ");
+    input = ReadLine();
+
+    if (!string.IsNullOrEmpty(input) && int.TryParse(input, out n))
+    {
+        digits = (int)Math.Log10(n) + 1;
+        if (3 <= digits && digits < 4)
+        {
+            WriteLine($"Последняя цифра числа '{n}': '{n%10}'");
+        }
+        else WriteLine("Некорректный ввод. Попробуйте ещё раз.");
+    }
+    else if (input == "q") break;
+    else WriteLine("Некорректный ввод. Попробуйте ещё раз."); 
+}
