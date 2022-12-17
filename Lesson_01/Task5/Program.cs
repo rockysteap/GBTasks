0cb7ca9b404c0a9ef78a4aee1a42bc32efd92fd7
Ilia Mediankin <i.mediankin@gmail.com>
2022-12-17 16:34:18 +0300

    feat: add Task5

---
+++ Lesson_01/Task5/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Task5. Напишите программу, которая на вход принимает одно число (N), 
+//         а на выходе показывает все целые числа в промежутке от -N до N.
+
+//         4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+//         2 -> " -2, -1, 0, 1, 2"
+
+using static System.Console;
+
+int i;
+int n = 1;
+string input = "";
+
+Clear();
+
+while (true)
+{
+    Write("Введите: 'q'-выход, или 'число'-для просмотра цепочки чисел от '-числа' до '+числа': ");
+    input = ReadLine();
+    
+    if (!string.IsNullOrEmpty(input) && int.TryParse(input, out n))
+    {
+        for (i=-n;i<n+1;i++) Console.Write($"{i} ");
+        WriteLine();
+    }
+    else if (input == "q") break;
+    else WriteLine("Некорректный ввод. Попробуйте ещё раз.");
+}
