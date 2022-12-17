075fb3534269301f09d1093e3f681793dfa2d8a0
Ilia Mediankin <i.mediankin@gmail.com>
2022-12-17 19:28:52 +0300

    feat(homework_01): add Task2-yagni

---
+++ Homework_01/Task2-yagni/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2:   Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//             Например: 1) a = 5; b = 7 -> max = 7 2) a = 2 b = 10 -> max = 10 3) a = -9 b = -3 -> max = -3
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите два числа, чтобы определить большее из них.");
+Write("Введите первое число 'a': ");
+int a = Convert.ToInt32(ReadLine());
+Write("Введите второе число 'b': ");
+int b = Convert.ToInt32(ReadLine());
+
+if (a > b) WriteLine($"a = {a}; b = {b} -> max = {a}");
+else if (a < b) WriteLine($"a = {a}; b = {b} -> max = {b}");
+else WriteLine($"a = {a}; b = {b} -> Числа равны!");
