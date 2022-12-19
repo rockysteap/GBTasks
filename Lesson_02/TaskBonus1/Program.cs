995108c8f42eb21c8d8e2cfac60527719d0366a9
Ilia Mediankin <i.mediankin@gmail.com>
2022-12-19 23:59:21 +0300

    feat(lesson_02): add TaskBonus1

---
+++ Lesson_02/TaskBonus1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Два числа равны или больше/меньше?
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите первое число 'a': ");
+int a = int.Parse(ReadLine());
+WriteLine("Введите второе число 'b': ");
+int b = int.Parse(ReadLine());
+
+// вариант 1
+// if (a==b) WriteLine(" = ");
+// else WriteLine(a<b?"<":">");
+
+// вариант 2
+WriteLine(a==b?"a = b":a<b?"a < b":"a > b");
