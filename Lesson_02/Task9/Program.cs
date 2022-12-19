28ab2eb71da238e01ab05269df527d2b811e266e
Ilia Mediankin <i.mediankin@gmail.com>
2022-12-19 23:40:46 +0300

    feat(lesson_02): add Task9

---
+++ Lesson_02/Task9/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+// Например:  1) 78 -> 8  2) 12-> 2  3) 85 -> 8
+
+using static System.Console;
+Clear();
+
+int number = new Random().Next(10, 100);
+WriteLine(number);
+int a1 = number/10;
+int a2 = number%10;
+int max=a1>a2?a1:a2; // тернарный оператор
+WriteLine(max);
+
+WriteLine(number/10>number%10?number/10:number%10);
+
+// if(a1>a2)
+// {
+//     WriteLine(a1);
+// }
+// else
+// {
+//     WriteLine(a2);
+// }
+
+// using static System.Console;
+// Clear();
+
+// int number=new Random().Next(10,100);
+// WriteLine(number);
+// WriteLine(number/10>number%10?number/10:number%10);
+
+// if(number/10>number%10)
+// {
+//     WriteLine(number/10);
+// }
+// else
+// {
+//     WriteLine(number%10);
+// }
