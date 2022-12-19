e9fbdb43f772cf7a9505d9083e6b94cb30267924
Ilia Mediankin <i.mediankin@gmail.com>
2022-12-20 00:27:48 +0300

    feat(lesson_02): add TaskBonus2

---
+++ Lesson_02/TaskBonus2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывести разницу между большим и меньшим из трех чисел.
+
+using static System.Console;
+Clear();
+
+int a = new Random().Next(100);
+int b = new Random().Next(100);
+int c = new Random().Next(100);
+
+int max = a<b?(b<c?c:b):a<c?c:a;
+int min = a>b?(b>c?c:b):a>c?c:a;
+
+WriteLine($"Число a = {a}, число b = {b}, число c = {c}, разница между мин ({min}) и макс ({max}) = {max-min}.");
+WriteLine();
