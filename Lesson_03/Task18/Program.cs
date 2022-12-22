961c53cfaa4f179cba26e46163d47a0473822f99
Ilia Mediankin <i.mediankin@gmail.com>
2022-12-23 02:57:03 +0300

    feat(lesson_03): add Task18

---
+++ Lesson_03/Task18/Program.cs
@@ -0,0 +1,43 @@
+﻿// 18. По номеру дня недели вывести его название при помощи switch() - case
+
+using static System.Console;
+Clear();
+
+Write("Введите число, чтобы получить наименование дня недели: ");
+int n = int.Parse(ReadLine()!);
+
+switch(n)
+{
+    case 1: {
+        WriteLine($"День недели № {n} - Понедельник");
+        break;
+    }
+    case 2: {
+        WriteLine($"День недели № {n} - Вторник");
+        break;
+    }
+    case 3: {
+        WriteLine($"День недели № {n} - Среда");
+        break;
+    }
+    case 4: {
+        WriteLine($"День недели № {n} - Четверг");
+        break;
+    }
+    case 5: {
+        WriteLine($"День недели № {n} - Пятница");
+        break;
+    }
+    case 6: {
+        WriteLine($"День недели № {n} - Суббота");
+        break;
+    }
+    case 7: {
+        WriteLine($"День недели № {n} - Воскресенье");
+        break;
+    }
+    default: {
+        WriteLine("Ошибка");
+        break;
+    }
+}
