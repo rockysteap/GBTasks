42fea233df94ab947377b54ec34e55e8b5d37c90
Ilia Mediankin <i.mediankin@gmail.com>
2022-12-23 03:08:43 +0300

    feat(homework_03): add Task19

---
+++ Homework_03/Task19/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 19: Не использовать обращение к символу строки, только математические методы работы с числами
+//            Напишите программу, которая принимает на вход пятизначное число и проверяет,
+//            является ли оно палиндромом.
+//            Например:  1) 14212 -> нет  2) 12821 -> да  3) 23432 -> да
+
+using static System.Console;
+Clear();
+
+Write("Введите число (до 10 символов) для проверки свойства палиндрома: ");
+int.TryParse(ReadLine(), out int n);
+
+int d = (int)Math.Log10(n) + 1;  // d - digits (кол-во цифр в числе)
+int i = 1;
+
+if (d % 2 == 0)
+{
+    while (i < d / 2)
+    {
+        if ((
+                Convert.ToInt32(n % (Math.Pow(10, (d - i))))
+                /
+                Convert.ToInt32(Math.Pow(10, (d - i - 1)))
+            ) != (
+                Convert.ToInt32(n % (Math.Pow(10, (i + 1))))
+                /
+                Convert.ToInt32(Math.Pow(10, (i)))
+        ))
+        {
+            WriteLine($"Число {n} не является палиндромом.");
+            return;
+        }
+        i++;
+    }
+}
+else if (d % 2 != 0)
+{
+    while (i < d / 2)
+    {
+        if ((
+                Convert.ToInt32(n % (Math.Pow(10, (d - i))))
+                /
+                Convert.ToInt32(Math.Pow(10, (d - i - 1)))
+            ) != (
+                Convert.ToInt32(n % (Math.Pow(10, (i + 1))))
+                /
+                Convert.ToInt32(Math.Pow(10, (i)))
+        ))
+        {
+            WriteLine($"Число {n} не является палиндромом.");
+            return;
+        }
+        i++;
+    }
+}
+
+WriteLine($"Число {n} является палиндромом.");
