4b8a872151706571de4072eb28577d30a726a70e
Ilia Mediankin <i.mediankin@gmail.com>
2023-01-16 01:43:50 +0300

    feat(lesson04): add Task28

---
+++ Lesson_04/Task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// 28. Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4  -> 24  // 5  -> 120
+
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int.TryParse(ReadLine(), out int number);
+
+WriteLine($"Произведение ряда чисел от 1 до {number} = {NumProduct(number)}");
+
+
+int NumProduct(int num)
+{
+    if (num <= 0) return 0;  // возвращаем 0 без вычислений, если ряд проходит через 0
+
+    int result = 1;
+    while (num > 0)
+    {
+        result *= num;
+        num--;
+    }
+    return result;
+}
