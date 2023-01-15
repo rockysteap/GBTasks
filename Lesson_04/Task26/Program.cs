679041950cada32b539b6f6ade8e5c4857566d0c
Ilia Mediankin <i.mediankin@gmail.com>
2023-01-16 01:42:44 +0300

    feat(lesson04): add Task26

---
+++ Lesson_04/Task26/Program.cs
@@ -0,0 +1,37 @@
+﻿// 26. Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3  // 78 -> 2  // 89126 -> 5
+
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int.TryParse(ReadLine(), out int number);
+
+
+WriteLine($"Количество цифр в числе {number} -> {NumCountWLoop(number)}");
+WriteLine($"Количество цифр в числе {number} -> {NumCountWLog(number)}");
+WriteLine($"Количество цифр в числе {number} -> {NumCountWLength(number)}");
+
+
+int NumCountWLoop(int num)
+{
+    int digits = 0;
+    while (num > 0)
+    {
+        digits++;
+        num /= 10;
+    }
+    return digits;
+}
+
+int NumCountWLog(int num)
+{
+    int digits = (int)Math.Log10(num) + 1;
+    return digits;
+}
+
+int NumCountWLength(int num)
+{
+    int digits = Convert.ToString(num).Length;
+    return digits;
+}
