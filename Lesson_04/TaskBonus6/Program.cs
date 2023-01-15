d2c7387d505f5079330afdee2c55fe408800fe7b
Ilia Mediankin <i.mediankin@gmail.com>
2023-01-16 01:45:38 +0300

    feat(lesson04): add TaskBonus6

---
+++ Lesson_04/TaskBonus6/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт сумму чисел от 1 до N, 
+// при условии, что N будет делиться на это число без остатка.
+// Иными словами: найти сумму всех целочисленных делителей числа N от 1 до N.
+
+
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int.TryParse(ReadLine(), out int number);
+
+
+WriteLine($"Сумма целочисленных делителей числа {number} = {DivisorSum(number)}");
+
+
+
+int DivisorSum(int num)
+{
+    int result = 0;
+    int number = num;
+    while (num > 0)
+    {
+        if (number % num == 0)
+        {
+            result += num;
+        }
+        num--;
+    }
+    return result;
+}
