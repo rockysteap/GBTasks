3e4b202df0ab6e6f1a80f4c5f29ea4ae0459538a
Ilia Mediankin <i.mediankin@gmail.com>
2023-01-26 18:58:39 +0300

    feat(lesson_07): add Task48

---
+++ Lesson_07/Task48/Program.cs
@@ -0,0 +1,41 @@
+﻿// **Задача 48:**  Задайте двумерный массив размера m на n, 
+//                 каждый элемент в массиве находится по формуле: 
+//                 Aₘₙ = m+n. Выведите полученный массив на экран.
+//                 Например: m = 3, n = 4.  0 1 2 3   1 2 3 4   2 3 4 5
+
+using static System.Console;
+Clear();
+
+Write("Введите через пробел размерность матрицы: ");
+string[] par = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int[,] matrixArray = CreateMatrixArray(int.Parse(par[0]), int.Parse(par[1]));
+
+PrintMatrixArray(matrixArray);
+
+
+int[,] CreateMatrixArray(int rows,  int columns)
+{
+    int[,] resultArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i, j] = i + j;
+        }
+    }
+    return resultArray;
+}
+
+
+void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],3} ");
+        }
+        WriteLine();
+    }
+}
