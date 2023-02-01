c3cb5028688a587a5ec6bbddb8aaee2c5e50f19a
Ilia Mediankin <i.mediankin@gmail.com>
2023-02-01 20:21:28 +0300

    feat(lesson_09): add Task63

---
+++ Lesson_09/Task63/Program.cs
@@ -0,0 +1,33 @@
+﻿// **Задача 63:**  Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+//                 в промежутке от 1 до N.
+//                 Например:  1) N = 5 -> "1, 2, 3, 4, 5"  2) N = 6 -> "1, 2, 3, 4, 5, 6"
+
+using static System.Console;
+
+string result = GetNumbers(5);
+WriteLine(result);
+PrintNumbers(5);
+WriteLine();
+WriteLine(GetNumbers1(5));
+
+
+string GetNumbers(int num)
+{
+    if (num == 1) return num.ToString();
+    else return $"{GetNumbers(num - 1)}, {num}";
+}
+
+string GetNumbers1(int num)
+{
+    return (num == 1) ? num.ToString() : $"{GetNumbers1(num - 1)}, {num}";
+}
+
+void PrintNumbers(int num)
+{
+    if (num == 1) Write(num);
+    else
+    {
+        PrintNumbers(num - 1);
+        Write($", {num}");
+    }
+}
