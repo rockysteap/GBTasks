a4c42b5eab547c94e447dcaf2970c8bd506d0575
Ilia Mediankin <i.mediankin@gmail.com>
2023-02-01 20:21:57 +0300

    feat(lesson_09): add Task67

---
+++ Lesson_09/Task67/Program.cs
@@ -0,0 +1,14 @@
+﻿// **Задача 67:** Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int result = GetNumbersSum(123456789);
+Console.WriteLine(result);
+// 123
+int GetNumbersSum(int num)
+{
+    if (num / 10 == 0) return num%10;
+    
+    else return GetNumbersSum(num/10) + num%10;
+
+}
